e5670d971ec33f471a731e099c4978e1abe6ffed
Tommie Behrenbeck <behrenbt@gmail.com>
2018-08-04 14:00:57 -0500

    editingJS

--- assets/app.js
+++ assets/app.js
@@ -17,56 +17,52 @@ $(document).ready(function () {
         gifsArray.push(newGif);
         renderButtons();
         return;
-    })
-});
-
-
-
-
-
-function gifButtonClicked() {
-    var userInput = $('#gif-input').val();
-    searchGif(userInput);
-}
-
-
-
-function searchGif(gifName) {
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=wLblm7yDyFoA5Am3Mb57uSYm2CRmsUUo';
-
-    $.ajax({
-        url: queryURL,
-        type: 'GET',
-    })
-        .then(function (response) {
-            displayGif(response);
-        })
-}
-
-
-function displayGif(response) {
-    $('#gifs').empty();
-    for (var i = 0; i < response.data.length; i++) {
-        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
-        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
-            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
-            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:auto; height:auto">';
-
-        // image = '<div class="col-md-4">' + image + "</div>";
-        $('#gifs').append(image);
-    }
-
-    $('.movImage').on('click', function () {
-        var state = $(this).attr('data-state');
-        if (state == 'still') {
-            $(this).attr('src', $(this).attr("data-animate"));
-            $(this).attr('data-state', 'animate');
-        } else {
-            $(this).attr('src', $(this).attr("data-still"));
-            $(this).attr('data-state', 'still');
-        }
-
     });
-}
 
-$(document).on("click", ".show", searchGif);
+    $("button").on("click", function () {
+		var gif = $(this).attr("data-gif");
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?q= ' + gif + ' &api_key=wLblm7yDyFoA5Am3Mb57uSYm2CRmsUUo&limit=5'
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            var results = response.data
+            $("#gifs").empty();
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+				var p = $("<p>").text("Rating: " + results[i].rating);
+				var gifImg = $("<img>");
+
+				gifImg.attr("src", results[i].images.original_still.url);
+				gifImg.attr("data-still", results[i].images.original_still.url);
+				gifImg.attr("data-animate", results[i].images.original.url);
+				gifImg.attr("data-state", "still");
+				gifImg.attr("class", "gif");
+				gifDiv.append(p);
+				gifDiv.append(gifImg);
+				$("#gifs").append(gifDiv);
+            }
+        })
+
+    })
+
+    function animation(){
+		var state = $(this).attr("data-state");
+		var animateImage = $(this).attr("data-animate");
+		var stillImage = $(this).attr("data-still");
+
+		if (state == "still") {
+			$(this).attr("src", animateImage);
+			$(this).attr("data-state", "animate");
+		}
+
+		else if (state == "animate") {
+			$(this).attr("src", stillImage);
+			$(this).attr("data-state", "still");
+		}
+    }
+    
+	$(document).on("click", ".gif", animation);
+
+})
